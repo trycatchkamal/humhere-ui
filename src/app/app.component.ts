41c406a58e17c4a523873b41705ceade3110fc1a
Kamal Sekar <kamal@greenfinch.local>
2018-04-16 07:51:35 +0100

    Landing page with sidenav and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule } from '@angular/router';
+import 'hammerjs';
+import {MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatDividerModule,
+  MatMenuModule,
+  MatTooltipModule,
+  MatSidenavModule,MatListModule,MatButtonToggleModule,
+} from '@angular/material';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule, MatButtonModule, MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule, MatTooltipModule,
+    MatCardModule, MatMenuModule, MatDividerModule, MatSidenavModule,
+    BrowserAnimationsModule, NgbModule.forRoot(), FormsModule, MatListModule, MatButtonToggleModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
